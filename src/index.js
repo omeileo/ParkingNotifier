fa8ed6144f7241d960ffd37318c28779550819b4
Jase-Omeileo West <omeileo@icloud.com>
2018-07-06 08:44:54 -0500

    (chore) Update folder structure.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// External Dependencies
+import { AppRegistry } from 'react-native'
+
+// Internal Dependencies
+import App from './App'
+import { name as appName } from '../app.json'
+
+AppRegistry.registerComponent(appName, () => App)
